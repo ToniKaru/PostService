d4acfedd42f7ba0964261eef1ef0e15ed1dfe266
ToniKaru <toni.karu@gmail.com>
2022-05-10 16:13:22 +0200

    Add PostRepository and update application.properties and .gitignore

--- src/main/java/se/iths/webservices/tonahs/postservice/PostServiceApplication.java
+++ src/main/java/se/iths/webservices/tonahs/postservice/PostServiceApplication.java
@@ -2,9 +2,11 @@ package se.iths.webservices.tonahs.postservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-public class PostServiceApplication {
+@EnableMongoRepositories
+public class PostServiceApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PostServiceApplication.class, args);
